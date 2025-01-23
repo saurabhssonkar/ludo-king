fbcbd650503ff8429007593cac7362122f6b6847
saurabhssonkar <saurabhssonkar@gmail.com>
2025-01-23 21:51:16 +0530

    Update FourTrangle.jsx

--- src/components/FourTrangle.jsx
+++ src/components/FourTrangle.jsx
@@ -6,7 +6,8 @@ import firework from '../../public/assets/animation/firework.json'
 
 const FourTrangle = () => {
     const size = 100
-    const [blast, setBlast] = useState(true);
+
+    const [blast, setBlast] = useState(false);
     return (
         <div className='flex justify-center items-center  w-[20%] h-full border overflow-hidden bg-white' style={{ borderColor: Colors.borderColor }}>
             {blast &&
@@ -19,8 +20,20 @@ const FourTrangle = () => {
 
             <svg height={size} width={size - 5}>
                 <polygon
-                    points={`0,0 ${size / 2},${size / 2} ${size / 2},0`}
+                    points={`0,0 ${size / 2},${size / 2} ${size },0`}
+                    fill={Colors.yellow}
+                />
+                 <polygon
+                    points={`${size},0 ${size}, ${size} ${size / 2} ${size / 2}`}
                     fill={Colors.blue}
+                />
+                <polygon
+                    points={` 0 ${size} ${size/2}, ${size/2} ${size}, ${size}`}
+                    fill={Colors.red}
+                />
+                 <polygon
+                    points={`0,0 ${size/2}, ${size/2} 0, ${size}`}
+                    fill={Colors.green}
                 />
             </svg>
 
